2e046afec3ffee23fc163558dfe34495cbb333bc
LeeSeokJune <indians1720@gmail.com>
2022-12-01 22:44:36 +0900

    test

--- js/petfood_select.js
+++ js/petfood_select.js
@@ -1,4 +1,9 @@
+
+
 var louis_url = 'http://52.79.233.120:8000/'; 
-function get_petfood_data(){
-    
+function show_petfood_data(){
+    var inner_text = '';
+    inner_text += 'asdf';
+    show_petfood.inner_txt = inner_text;
 }
+
